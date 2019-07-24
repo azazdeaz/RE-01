87a0197eadae745001c518dd61daf58d60c0d826
Polgár András <azazdeaz@gmail.com>
2019-07-24 11:07:24 +0200

    :alembic: teleop motors

---
+++ launchers/drive.ts
@@ -0,0 +1,74 @@
+import ioHook from 'iohook'
+ioHook.start(false)
+
+import zmq from 'zeromq'
+const sock = zmq.socket('req');
+
+sock.connect('tcp://192.168.0.103:5556');
+
+enum KeyCodes {
+    UP = '57416',
+    RIGHT = '57421',
+    DOWN = '57424',
+    LEFT = '57419',
+    LIGHT = '38',
+}
+
+const state = {
+    [KeyCodes.UP]: false,
+    [KeyCodes.RIGHT]: false,
+    [KeyCodes.DOWN]: false,
+    [KeyCodes.LEFT]: false,
+    [KeyCodes.LIGHT]: false,
+}
+
+type Event = {
+    keycode: number,
+    type: 'keydown' | 'keyup'
+}
+
+const listener = (e: Event) => {
+    const code = e.keycode.toString()
+    console.log(code)
+    if (code in state) {
+        state[code as KeyCodes] = e.type === 'keydown'
+        sendMotorCommand()
+        sendLedCommand()
+    }
+}
+
+ioHook.on('keydown', listener)
+ioHook.on('keyup', listener)
+
+const sendMotorCommand = () => {
+    let left = 0
+    let right = 0
+    if (state[KeyCodes.UP]) {
+        left += 1
+        right += 1
+    }
+    if (state[KeyCodes.DOWN]) {
+        left -= 1
+        right -= 1
+    }
+    if (state[KeyCodes.LEFT]) {
+        left += 1
+    }
+    if (state[KeyCodes.RIGHT]) {
+        right += 1
+    }
+    const norm = left !== 0 || right !== 0
+        ? 1 / Math.max(Math.abs(left), Math.abs(right))
+        : 0
+    left *= norm
+    right *= norm
+    const msg = `motor speed ${left} ${right}`
+    console.log(msg)
+    sock.send(msg)
+}
+
+const sendLedCommand = () => {
+    const msg = `led ${state[KeyCodes.LIGHT] ? 'on' : 'off'}`
+    console.log(msg)
+    sock.send(msg)
+}
--- launchers/test1.py
+++ launchers/test1.py
@@ -2,8 +2,9 @@ import zmq
 import random
 import sys
 import time
-from gpiozero import LED
+from gpiozero import LED, Robot
 
+robot = Robot(left=(19, 13), right=(18, 12))
 led = LED(26)
 
 port = "5556"
@@ -16,7 +17,7 @@ while True:
     #  Wait for next request from client
     message = socket.recv()
     print("Received request: ", message)
-    domain, command = message.split()
+    domain, command, *args = message.split()
     print(domain, command)
     if domain  == 'led'.encode('UTF-8'):
         print('LED')
@@ -26,4 +27,8 @@ while True:
         elif command == 'off'.encode('UTF-8'):
             print('OFF')
             led.off()
+    elif domain  == 'motor'.encode('UTF-8'):
+        print('MOTOR')
+        if command == 'speed'.encode('UTF-8'):
+            robot.value = (float(args[0]), float(args[1]))
     socket.send(("World from %s" % port).encode('ascii'))
