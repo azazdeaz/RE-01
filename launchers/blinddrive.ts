8b188d340e86321bdca4ea6e75068ae2aa68657f
Polgár András <azazdeaz@gmail.com>
2019-07-28 14:45:11 +0200

    :alembic: test ustuck process

--- launchers/blinddrive.ts
+++ launchers/blinddrive.ts
@@ -6,22 +6,69 @@ const sockReq = zmq.socket('req')
 sockReq.connect('tcp://192.168.0.103:5556')
 console.log('listening...')
 
+const SEC = 1000
+
 const send = (msg: string) => {
-    console.log(msg)
-    sockReq.send(msg)
+  console.log(msg)
+  sockReq.send(msg)
 }
 
-sockSub.on('message', function(message) {
-    console.log('received a message:', message.toString());
-    const [topic, value] = message.toString().split(' ')
+const { react, sense } = (() => {
+  const AUTO_MAX_SPEED = 0.6
+  let stuckSince: null | number = null
+  let obstacleDangerLevel = 0
+
+  const sense = (topic: string, data: any) => {
     if (topic === 'obstacle') {
-        const distance = parseFloat(value)
-        let left = 0.6
-        let right = 0.6
-        if (distance < 0.4) {
-            // slow down the right wheel as the obstacle getting closer
-            right *= Math.max(0, distance - 0.3) * 10
+      const { distance } = data
+      obstacleDangerLevel = Math.min(1, Math.max(0, distance * -100 + 4))
+    } else if (topic === 'mpu') {
+      const { ax, ay, az } = data
+      console.log({ ax, ay, az } )
+      if (Math.abs(ax) < 0.2 && Math.abs(ay) < 0.2 && Math.abs(az) < 0.2) {
+        if (!stuckSince) {
+          stuckSince = Date.now()
         }
-        send(`motor speed ${left} ${right}`)
+      }
+      else {
+        stuckSince = null
+      }
     }
-});
+  }
+
+  const react = () => {
+    let speedLeft = 0
+    let speedRight = 0
+    console.log('>>>', stuckSince && Date.now() - stuckSince,  3 * SEC)
+    if (stuckSince && Date.now() - stuckSince > 3 * SEC) {
+      speedLeft = -AUTO_MAX_SPEED
+      speedRight = -AUTO_MAX_SPEED
+    }
+    else if (obstacleDangerLevel > 0) {
+      speedLeft = AUTO_MAX_SPEED * -obstacleDangerLevel
+      speedRight = AUTO_MAX_SPEED
+    }
+    else {
+      speedLeft = AUTO_MAX_SPEED
+      speedRight = AUTO_MAX_SPEED
+    }
+    send(`motor speed ${speedLeft} ${speedRight}`)
+  }
+
+  return { sense, react }
+})()
+
+sockSub.on('message', function(message) {
+  message = message.toString()
+  // console.log('received a message:', message)
+  const topicEnd = message.indexOf(' ')
+  if (topicEnd < 0) {
+    console.warn('Unknown message format')
+    return
+  }
+  const topic = message.substring(0, topicEnd)
+  const data = JSON.parse(message.substring(topicEnd + 1))
+  sense(topic, data)
+})
+
+setInterval(() => react(), 100)
--- launchers/distance.py
+++ launchers/distance.py
@@ -1,15 +1,11 @@
-import zmq
-from gpiozero import DistanceSensor
 from time import sleep
+from gpiozero import DistanceSensor
+import json
 
-port = "5557"
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:%s" % port)
-
-sensor = DistanceSensor(echo=20, trigger=21, max_distance=10)
-while True:
-    print('Value: ', sensor.value)
-    print('Distance: ', sensor.distance, ' / ', sensor.max_distance)
-    socket.send(('obstacle %f' % sensor.distance).encode('ascii'))
-    sleep(0.1)
+def run(publish):
+    sensor = DistanceSensor(echo=20, trigger=21, max_distance=10)
+    while True:
+        print('Value: ', sensor.value)
+        print('Distance: ', sensor.distance, ' / ', sensor.max_distance)
+        publish('obstacle', { 'distance': sensor.value })
+        sleep(0.1)
